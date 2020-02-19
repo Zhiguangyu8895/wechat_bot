a93d3ef30a9eca579eb5a8a8b15822f3974e0bdd
unknown <yu.bestronger@gmail.com>
2020-02-19 10:37:03 +0900

    fix qr-code and add sitecustomize.py

---
+++ sitecustomize.py
@@ -0,0 +1,3 @@
+#/home/mgxpppit/.local/lib/python2.7/site-packages
+import sys
+sys.setdefaultencoding("utf-8")
--- wechat_regular_bot.py
+++ wechat_regular_bot.py
@@ -5,7 +5,7 @@ from itchat.content import *
 from datetime import datetime, timedelta
 
 if __name__ == '__main__':
-    itchat.auto_login(enableCmdQR=True)
+    itchat.auto_login(enableCmdQR=2)
     itchat.run()
 
 # select friend who you want to send regular messages to
@@ -16,7 +16,7 @@ userName = users[0]['UserName']
 while 1:
     now = datetime.now()
     now_str = now.strftime('%Y/%m/%d %H:%M:%S')[11:]
-    if now_str in ['18:00:05']:
-        itchat.send('[Strong]', toUserName = userName)
-    time.sleep(1)
+    if now_str in ['18:00:00']:
+        itchat.send('/强', toUserName = userName)
+        time.sleep(86350)
 
